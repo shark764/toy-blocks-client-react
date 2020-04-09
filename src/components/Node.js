d8f40b3416fdf24c9704188909187962c86c1eaa
Carlos Angarita <cangarita@fullstacklabs.co>
2020-04-09 18:24:45 -0500

    New visual design with Material UI

--- src/components/Node.js
+++ src/components/Node.js
@@ -1,108 +1,64 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
-import colors from '../constants/colors';
-import Status from './Status';
+import React from "react";
+import PropTypes from "prop-types";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import {
+  ExpansionPanel,
+  ExpansionPanelSummary,
+  Typography,
+  ExpansionPanelDetails,
+  makeStyles,
+  Box,
+} from "@material-ui/core";
+import colors from "../constants/colors";
 
-const Wrapper = styled.div`
-  margin: 10px 0;
-  padding: 10px;
-  border: 1px solid ${colors.border};
-  background: ${colors.contentBackground};
-  border-radius: 5px;
-`;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    margin: "16px 0",
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    display: "block",
+  },
+  secondaryHeading: {
+    fontSize: theme.typography.pxToRem(12),
+    color: colors.faded,
+  },
+}));
 
-const Head = styled.div`
-  display: flex;
-  justify-content: space-between;
-`;
-
-const Body = styled.div`
-  display: flex;
-  justify-content: space-between;
-`;
-
-const Name = styled.div`
-  margin-bottom: 6px;
-  color: ${colors.primary};
-  font-size: 16px;
-  font-weight: 700;
-`;
-
-const URL = styled.div`
-  margin-top: 2px;
-  margin-bottom: 2px;
-  color: ${colors.faded};
-  font-weight: 500;
-`;
-
-const Toolbar = styled.div`
-  display: flex;
-  justify-content: right;
-`;
-
-const Expander = styled.button`
-  margin-left: 10px;
-  margin-top: 7px;
- 
-  background: none;
-  border: none;
-  position: relative;
-  
-  cursor: pointer;
-  outline: none;
-  
-  &:before {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    border-${props => props.expanded ? 'top' : 'bottom'}: 7px solid ${colors.text};
-    border-left: 7px solid transparent;
-    border-right: 7px solid transparent;
-  }
-`;
-
-const BlocksWrapper = styled.div`
-  margin: 20px 0;
-  text-align: center;
-  color: ${colors.danger};
-  font-weight: 700;
-`;
-
-const Node = ({node, expanded, toggleNodeExpanded}) => (
-  <Wrapper>
-    <Head>
-      <Name>
-        {node.name || 'Unknown'}
-      </Name>
-      <Toolbar>
-        <Status {...node} />
-        <Expander expanded={expanded} onClick={() => toggleNodeExpanded(node)}/>
-      </Toolbar>
-    </Head>
-    <Body>
-      <URL>
-        {node.url}
-      </URL>
-    </Body>
-    {expanded && (
-      <BlocksWrapper>
-        Blocks go here
-      </BlocksWrapper>
-    )}
-  </Wrapper>
-);
+const Node = ({ node, expanded, toggleNodeExpanded }) => {
+  const classes = useStyles();
+  return (
+    <ExpansionPanel
+      className={classes.root}
+      expanded={expanded}
+      onChange={() => toggleNodeExpanded(node)}
+    >
+      <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+        <Box>
+          <Typography className={classes.heading}>
+            {node.name || "Unknown"}
+          </Typography>
+          <Typography className={classes.secondaryHeading}>
+            {node.url}
+          </Typography>
+        </Box>
+      </ExpansionPanelSummary>
+      <ExpansionPanelDetails>
+        <Typography>Blocks go here</Typography>
+      </ExpansionPanelDetails>
+    </ExpansionPanel>
+  );
+};
 
 Node.propTypes = {
   node: PropTypes.shape({
     url: PropTypes.string,
     online: PropTypes.bool,
     name: PropTypes.string,
-    loading: PropTypes.bool
+    loading: PropTypes.bool,
   }).isRequired,
   expanded: PropTypes.bool,
-  toggleNodeExpanded: PropTypes.func.isRequired
+  toggleNodeExpanded: PropTypes.func.isRequired,
 };
 
 export default Node;
--- src/containers/Nodes.js
+++ src/containers/Nodes.js
@@ -1,65 +1,66 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
-import * as actions from '../actions/nodes';
-import Node from '../components/Node';
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as actions from "../actions/nodes";
+import Node from "../components/Node";
+import { Typography, Box } from "@material-ui/core";
 
 export class Nodes extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      expandedNodeURL: null
+      expandedNodeURL: null,
     };
     this.toggleNodeExpanded = this.toggleNodeExpanded.bind(this);
   }
 
   componentDidMount() {
-    this.props.actions.checkNodeStatuses(this.props.nodes.list)
+    this.props.actions.checkNodeStatuses(this.props.nodes.list);
   }
 
   toggleNodeExpanded(node) {
     this.setState({
-      expandedNodeURL: node.url === this.state.expandedNodeURL ? null : node.url
+      expandedNodeURL:
+        node.url === this.state.expandedNodeURL ? null : node.url,
     });
   }
 
   render() {
-    const {nodes} = this.props;
+    const { nodes } = this.props;
     return (
-      <div>
-        <h1>Nodes</h1>
-        {nodes.list.map(node =>
+      <Box paddingTop={2}>
+        <Typography variant="h4" component="h1">
+          <strong>Nodes</strong>
+        </Typography>
+        {nodes.list.map((node) => (
           <Node
             node={node}
             key={node.url}
             expanded={node.url === this.state.expandedNodeURL}
             toggleNodeExpanded={this.toggleNodeExpanded}
           />
-        )}
-      </div>
+        ))}
+      </Box>
     );
   }
 }
 
 Nodes.propTypes = {
   actions: PropTypes.object.isRequired,
-  nodes: PropTypes.object.isRequired
+  nodes: PropTypes.object.isRequired,
 };
 
 function mapStateToProps(state) {
   return {
-    nodes: state.nodes
+    nodes: state.nodes,
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    actions: bindActionCreators(actions, dispatch)
+    actions: bindActionCreators(actions, dispatch),
   };
 }
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Nodes);
+export default connect(mapStateToProps, mapDispatchToProps)(Nodes);
