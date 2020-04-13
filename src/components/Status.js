e42a51fc8aaf3cd997cb203830d752620001aafc
Carlos Angarita <cangarita@fullstacklabs.co>
2020-04-13 11:37:58 -0500

    Update Status design

--- src/components/Status.js
+++ src/components/Status.js
@@ -1,37 +1,45 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Chip, CircularProgress, makeStyles } from "@material-ui/core";
+import { makeStyles, Box } from "@material-ui/core";
 import colors from "../constants/colors";
 
 function Status({ online, loading }) {
   const classes = useStyles({ online, loading });
   return (
-    <Chip
-      variant="outlined"
-      className={classes.root}
-      classes={{ icon: classes.icon }}
-      icon={loading && <CircularProgress size={20} color="inherit" />}
-      label={loading ? "Loading" : online ? "Online" : "Offline"}
-    />
+    <Box display="flex" alignItems="center">
+      <span className={classes.dot}></span>
+      <span className={classes.text}>
+        {loading ? "LOADING" : online ? "ONLINE" : "OFFLINE"}
+      </span>
+    </Box>
   );
 }
 
-const useStyles = makeStyles({
-  root: ({ online, loading }) => {
+const useStyles = makeStyles((theme) => ({
+  dot: ({ online, loading }) => {
     const color = loading
       ? colors.warning
       : online
       ? colors.success
       : colors.danger;
     return {
-      borderColor: color,
-      color,
+      borderRadius: "50%",
+      display: "inline-block",
+      width: 5,
+      height: 5,
+      backgroundColor: color,
     };
   },
-  icon: {
-    color: colors.warning,
-  },
-});
+  text: ({ online }) => ({
+    fontSize: theme.typography.pxToRem(14),
+    display: "block",
+    lineHeight: 1.5,
+    fontWeight: "400",
+    letterSpacing: 1,
+    paddingLeft: 5,
+    color: online ? colors.text : colors.faded,
+  }),
+}));
 
 Status.propTypes = {
   online: PropTypes.bool,
